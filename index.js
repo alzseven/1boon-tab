142957d9e66ad99e3812f47c22278a192f79f6cc
alzseven <jhp7140@gmail.com>
2021-04-25 05:11:41 +0900

    prettier

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const BASEURL = "http://advanced-webapps-class.github.io/classroom/2021/test1/";
-const PARAM_RECENT = "recent.json";
-const PARAM_HOT = "view.json";
-const PARAM_POPULAR = "popular.json";
-const $tabs = document.querySelector(".nav-tabs").children;
-const $list = document.getElementById("list");
+const BASEURL = 'http://advanced-webapps-class.github.io/classroom/2021/test1/';
+const PARAM_RECENT = 'recent.json';
+const PARAM_HOT = 'view.json';
+const PARAM_POPULAR = 'popular.json';
+const $tabs = document.querySelector('.nav-tabs').children;
+const $list = document.getElementById('list');
 const $loadingIcon = document
-  .getElementById("list")
-  .querySelector(".text-center");
-const $showMoreBtn = document.querySelector(".btn");
+  .getElementById('list')
+  .querySelector('.text-center');
+const $showMoreBtn = document.querySelector('.btn');
 
 const tabs = [
   { DOM: $tabs[0], PARAM: PARAM_RECENT },
@@ -20,40 +20,40 @@ let loadingTimer;
 
 function hideContents() {
   $list.childNodes.forEach((n) => {
-    if (n != $loadingIcon) {
+    if (n !== $loadingIcon) {
       $list.removeChild(n);
     }
   });
 }
 
 function setList(json) {
-  let node = document.createElement("div");
-  node.className = "list-group";
+  const node = document.createElement('div');
+  node.className = 'list-group';
 
   for (let i = 0; i < json.length; i += 1) {
-    const listItem = document.createElement("a");
-    listItem.href = `${json[i]["url"]}`;
-    listItem.className = "list-group-item";
-    listItem.classList.add("list-group-item-action");
+    const listItem = document.createElement('a');
+    listItem.href = `${json[i].url}`;
+    listItem.className = 'list-group-item';
+    listItem.classList.add('list-group-item-action');
 
-    const img = document.createElement("img");
-    img.src = `${json[i]["img"]}`;
-    img.className = "img";
-    img.classList.add("img-thumbnail");
+    const img = document.createElement('img');
+    img.src = `${json[i].img}`;
+    img.className = 'img';
+    img.classList.add('img-thumbnail');
 
-    const title = document.createElement("p");
-    title.className = "mb-1";
-    title.innerText = `${json[i]["title"]}`;
+    const title = document.createElement('p');
+    title.className = 'mb-1';
+    title.innerText = `${json[i].title}`;
 
-    const cp = document.createElement("small");
-    cp.innerText = `${json[i]["cp"]}`;
+    const cp = document.createElement('small');
+    cp.innerText = `${json[i].cp}`;
 
     listItem.appendChild(img);
     listItem.appendChild(title);
     listItem.appendChild(cp);
     node.appendChild(listItem);
   }
-  $list.appendChild(node);
+  $list.prepend(node);
 }
 
 function getData(url) {
@@ -65,15 +65,21 @@ function getData(url) {
 }
 
 function showLoading() {
-  $loadingIcon.style.display = "";
+  $loadingIcon.style.display = '';
 }
 
 function hideLoading() {
-  $loadingIcon.style.display = "none";
+  $loadingIcon.style.display = 'none';
+}
+
+function activateSelectedTab(tab) {
+  selectedTab.DOM.className = '';
+  selectedTab = tab;
+  selectedTab.DOM.className = 'active';
 }
 
 for (let i = 0; i < tabs.length; i += 1) {
-  tabs[i].DOM.addEventListener("click", (event) => {
+  tabs[i].DOM.addEventListener('click', () => {
     showLoading();
     hideContents();
     setTimeout(() => {
@@ -82,12 +88,6 @@ for (let i = 0; i < tabs.length; i += 1) {
       activateSelectedTab(tabs[i]);
     }, 1000);
   });
-}
-
-function activateSelectedTab(tab) {
-  selectedTab.DOM.className = "";
-  selectedTab = tab;
-  selectedTab.DOM.className = "active";
 }
 
 hideLoading();
